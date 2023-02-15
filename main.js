554534ba3e6c126eb6e42373d480d18136d09283
cafesito <leliasgomer@gmail.com>
2023-02-15 11:58:38 -0500

    uploaded

--- main.js
+++ main.js
@@ -4,10 +4,10 @@ const list = document.getElementById('list')
 btn.addEventListener('click', ()=>{
     list.classList.toggle('show')
     if (list.classList.contains('show')) {
-        list.classList.replace('max-[500px]:top-[-300%]', 'max-[500px]:top-[0]')
+        list.classList.replace('max-[500px]:top-[-400%]', 'max-[500px]:top-[0]')
         btn.setAttribute('name', 'close')
     }else{
-        list.classList.replace('max-[500px]:top-[0]', 'max-[500px]:top-[-300%]')
+        list.classList.replace('max-[500px]:top-[0]', 'max-[500px]:top-[-400%]')
         btn.setAttribute('name', 'grid-outline')
     }
 })
